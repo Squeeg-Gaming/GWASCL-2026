c8bcbde6a952fc9e774de80e612e1b3ff6455537
delgadojackson731-hub <delgadojackson731@gmail.com>
2026-09-06 02:21:30 -0400

    Update score.js

--- js/score.js
+++ js/score.js
@@ -14,7 +14,7 @@ export function score(rank, percent, minPercent) {
     if (rank > 900) {
         return 0;
     }
-    if (rank > 400 && percent < 100) {
+    if (rank > 350 && percent < 100) {
         return 0;
     }
 
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-24.8*Math.pow(rank-1, 0.4) + 350) *
+    let score = (-24.8*Math.pow(rank-1, 0.4) + 400) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
