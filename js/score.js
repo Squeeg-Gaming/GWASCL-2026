d519c2c3d941257e38fa14734a3927e81ef26ab1
delgadojackson731-hub <delgadojackson731@gmail.com>
2026-09-06 02:19:26 -0400

    Update score.js

--- js/score.js
+++ js/score.js
@@ -14,7 +14,7 @@ export function score(rank, percent, minPercent) {
     if (rank > 900) {
         return 0;
     }
-    if (rank > 350 && percent < 100) {
+    if (rank > 400 && percent < 100) {
         return 0;
     }
 
